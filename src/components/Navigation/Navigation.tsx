76a95fbeac31d39eb1bcd04f6a6d6bd55cf9e21a
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-08 15:17:35 +0100

    added styles to nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,32 +4,68 @@ import {
   faSquarePlus,
   faHeart,
 } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import "@fontsource/montserrat-alternates";
 
-const Navigation = () => {
+export const Navbar = styled.nav`
+  background-color: #157a6e;
+  height: 69px;
+  width: 100vw;
+  position: fixed;
+  align-items: center;
+  display: flex;
+
+  & ul {
+    display: flex;
+    list-style: none;
+    justify-content: space-around;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+
+    & li {
+      font-size: 22px;
+      font-family: Montserrat Alternates;
+      font-weight: bold;
+
+      & a {
+        color: #ffffff;
+        text-decoration: none;
+      }
+      & span {
+        color: #eaa92a;
+      }
+    }
+  }
+`;
+
+const Navigation = (): JSX.Element => {
   return (
     <>
-      <nav className="nav-bar">
+      <Navbar className="nav-bar">
         <ul>
-          <li>
+          <li className="account">
             <a href="/register">
               <FontAwesomeIcon icon={faUser} />
             </a>
           </li>
           <li>
-            <a href="/home">Videogames</a>
+            <a href="/home">
+              <span>Video</span>games
+            </a>
           </li>
           <li>
             <a href="/liked">
               <FontAwesomeIcon icon={faHeart} />
             </a>
           </li>
-          <li>
+          <li className="logo">
             <a href="/create-videogame">
               <FontAwesomeIcon icon={faSquarePlus} />
             </a>
           </li>
         </ul>
-      </nav>
+      </Navbar>
     </>
   );
 };
