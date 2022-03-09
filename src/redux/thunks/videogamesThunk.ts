894706dae0ff9c9204b745d8eb2b0a35ba5d27a2
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-09 18:10:57 +0100

    added thunks

---
+++ src/redux/thunks/videogamesThunk.ts
@@ -0,0 +1,12 @@
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { loadVideogamesAction } from "../actions/actionsCreators";
+
+export const loadVideogamesThunk = async (
+  dispatch: ThunkDispatch<void, unknown, AnyAction>
+) => {
+  const response = await fetch(`${process.env.REACT_APP_URLAPI}videogames`);
+  const videogamesList = await response.json();
+  const allVideogames = videogamesList.videogames;
+  dispatch(loadVideogamesAction(allVideogames));
+};
