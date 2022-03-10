8427cf9916d829dc9c018ed54aee84a1e2fce5fc
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-10 14:42:31 +0100

    added HomePage

--- src/components/VideogamesList/VideogamesList.tsx
+++ src/components/VideogamesList/VideogamesList.tsx
@@ -1,23 +1,11 @@
 import { Videogame } from "../../interfaces/Videogame";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { loadVideogamesThunk } from "../../redux/thunks/videogamesThunk";
 import VideogameCard from "../VideogameCard/VideogameCard";
-import { RootState } from "../../redux/store";
 
 const VideogamesList = (videogames: Videogame[]): JSX.Element => {
-  const dispatch = useDispatch();
-  const videogamesList = useSelector(
-    (state: RootState) => state.loadVideogames
-  );
-
-  useEffect(() => {
-    dispatch(loadVideogamesThunk);
-  }, [dispatch]);
   return (
     <>
       <ul>
-        {videogamesList.map((videogame: Videogame) => {
+        {videogames.map((videogame: Videogame) => {
           return <VideogameCard videogame={videogame} />;
         })}
       </ul>
