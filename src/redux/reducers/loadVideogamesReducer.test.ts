7a770c0b4646d37d8be6f16c0b98ac3549906ebf
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-10 14:59:56 +0100

    added test for default case at loadVideogamesReducer

--- src/redux/reducers/loadVideogamesReducer.test.ts
+++ src/redux/reducers/loadVideogamesReducer.test.ts
@@ -37,4 +37,46 @@ describe("Given a loadVideogamesReducer function", () => {
       expect(newState).toEqual(videogames);
     });
   });
+
+  describe("When it's called with an nonexistent action", () => {
+    test("Then it should return a new state and has to be the same as the initial state)", () => {
+      const currentVideogames: Videogame[] = [
+        {
+          name: "Apex Legends",
+          image: "https://www.xtrafondos.com/descargar.php?id=3030&vertical=1",
+          platforms: ["PS4", "XBOX", "PS5", "PC"],
+          genre: "Shooter",
+          description:
+            "A free-to-play strategic battle royale game featuring 60-player matches and team-based play",
+          year: 2019,
+        },
+        {
+          name: "GTA V",
+          image: "https://www.xtrafondos.com/descargar.php?id=3228&vertical=1",
+          platforms: ["PS4", "XBOX", "PS5", "PC"],
+          genre: "Action",
+          description:
+            "An action-adventure game played from either a third-person or first-person perspective. Players complete missions—linear scenarios with set objectives—to progress through the story. Outside of the missions, players may freely roam the open world.",
+          year: 2013,
+        },
+      ];
+
+      const action = {
+        type: "none",
+        videogames: currentVideogames,
+      };
+
+      const newState = loadVideogamesReducer(currentVideogames, action);
+
+      expect(newState).toEqual(currentVideogames);
+    });
+  });
+
+  describe("When it's called without state nor action", () => {
+    test("Then it should return an empty array as new state", () => {
+      const newState = loadVideogamesReducer();
+
+      expect(newState).toHaveLength(0);
+    });
+  });
 });
