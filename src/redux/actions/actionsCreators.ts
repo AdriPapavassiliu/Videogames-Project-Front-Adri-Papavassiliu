a09e14eaad4c202d15ed28498d4190f662cd2078
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-12 16:03:10 +0100

    added Action interfaces

--- src/redux/actions/actionsCreators.ts
+++ src/redux/actions/actionsCreators.ts
@@ -1,12 +1,20 @@
+import {
+  DeleteVideogameActionInterface,
+  LoadVideogamesActionInterface,
+} from "../../interfaces/Action";
 import { Videogame } from "../../interfaces/Videogame";
 import actionsTypes from "./actionsTypes";
 
-export const loadVideogamesAction = (videogames: Videogame[]) => ({
+export const loadVideogamesAction = (
+  videogames: Videogame[]
+): LoadVideogamesActionInterface => ({
   type: actionsTypes.loadVideogames,
   videogames,
 });
 
-export const deleteVideogameAction = (videogameId: string) => ({
+export const deleteVideogameAction = (
+  videogameId: string
+): DeleteVideogameActionInterface => ({
   type: actionsTypes.deleteVideogame,
   videogameId,
 });
