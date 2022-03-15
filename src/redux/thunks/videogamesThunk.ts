dbe4b7a90c6c0051f335f024fe97927bd152136f
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-15 16:10:05 +0100

    added addVideogameThunk and tested it

--- src/redux/thunks/videogamesThunk.ts
+++ src/redux/thunks/videogamesThunk.ts
@@ -1,6 +1,8 @@
 import { AnyAction } from "redux";
 import { ThunkDispatch } from "redux-thunk";
+import { Videogame } from "../../interfaces/Videogame";
 import {
+  addVideogameAction,
   deleteVideogameAction,
   loadVideogamesAction,
 } from "../actions/actionsCreators";
@@ -25,5 +27,25 @@ export const deleteVideogameThunk =
     );
     if (response.ok) {
       dispatch(deleteVideogameAction(videogameId));
+    }
+  };
+
+export const addVideogameThunk =
+  (videogame: Videogame) =>
+  async (dispatch: ThunkDispatch<void, unknown, AnyAction>) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_URLAPI}videogames/create`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(videogame),
+      }
+    );
+    const responseVideogame = await response.json();
+
+    if (responseVideogame.message === "Videogame created") {
+      dispatch(addVideogameAction(responseVideogame.videogame));
     }
   };
--- src/redux/thunks/videogamesThunks.test.ts
+++ src/redux/thunks/videogamesThunks.test.ts
@@ -1,4 +1,8 @@
-import { deleteVideogameThunk, loadVideogamesThunk } from "./videogamesThunk";
+import {
+  addVideogameThunk,
+  deleteVideogameThunk,
+  loadVideogamesThunk,
+} from "./videogamesThunk";
 
 describe("Given a loadVideogamesThunk function", () => {
   describe("When it is called", () => {
@@ -34,6 +38,28 @@ describe("Given a deleteVideogameThunk function", () => {
       await innerThunk(dispatch);
 
       expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given an addVideogameThunk function", () => {
+  describe("When it is called with a videogame", () => {
+    test("Then it should dispatch a function", async () => {
+      const videogame = {
+        name: "Apex Legends",
+        image: "https://www.xtrafondos.com/descargar.php?id=3030&vertical=1",
+        platforms: ["PS4", "XBOX", "PS5", "PC"],
+        genre: "Shooter",
+        description:
+          "A free-to-play strategic battle royale game featuring 60-player matches and team-based play",
+        year: 2019,
+      };
+      const dispatch = jest.fn();
+
+      const innerThunk = await addVideogameThunk(videogame);
+      await innerThunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
     });
   });
 });
