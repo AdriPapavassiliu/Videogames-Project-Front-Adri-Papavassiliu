bb56da80428ea416973cd7d4b09f03d14fd1e96d
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-03-18 17:21:11 +0100

    added LoginForm component

---
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../redux/store";
+import LoginForm from "./LoginForm";
+
+describe("Given a VideogameForm component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should render a button with 'Log in' as accessible name", () => {
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <LoginForm />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const loginButton = screen.getByRole("button", { name: /log in/i });
+
+      expect(loginButton).toBeInTheDocument();
+    });
+  });
+});
